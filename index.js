3fcbd4b9d30e6cd8593f6def37c60ae75f7e0428
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2023-01-07 15:59:27 -0600

    Fix issue with couchbase failing with SIGSEGV

--- index.js
+++ index.js
@@ -3,35 +3,45 @@ const couchbase = require("couchbase");
 const bodyParser = require("body-parser");
 
 const cluster = new couchbase.Cluster("couchbase://localhost");
-const bucket = cluster.bucket("default");
+
+const connect = async () => {
+    return cluster.bucket("uznum", {
+        "username": process.env.COUCHBASE_USER,
+        "password": process.env.COUCHBASE_USER_PWD
+    });
+}
+
+const connection = connect();
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/person/:id", (req, res) => {
-    bucket.get(req.params.id, (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.send(result);
+connection.then((bucket) => {
+    app.get("/person/:id", (req, res) => {
+        bucket.get(req.params.id, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.send(result);
+        });
     });
-});
 
-app.post("/person/:id", (req, res) => {
-    const person = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName
-    }
-    bucket.upsert(req.params.id, document,  (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
+    app.post("/person/:id", (req, res) => {
+        const person = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName
         }
-        res.send(result);
+        bucket.upsert(req.params.id, document,  (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.send(result);
+        });
     });
-});
 
-const server = app.listen(3003, () => {
-    console.log("Listening on port %s...", server.address().port);
+    const server = app.listen(3003, () => {
+        console.log("Listening on port %s...", server.address().port);
+    });
 });
